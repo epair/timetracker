9d68b3ffe5ea38d20a6df47be23ef31c6014fccb
Emmett Pair <elpair@gmail.com>
2021-03-05 14:38:17 -0500

    adds Project#name uniqueness validation

--- lib/models/project.rb
+++ lib/models/project.rb
@@ -1,8 +1,10 @@
 require_relative '../support/active_record_connector'
 
 class Project < ActiveRecord::Base
-  validates :name, presence: true
   has_many :project_tags
   has_many :tags, through: :project_tags
   has_many :entries
+
+  validates :name, presence: true
+  validates_uniqueness_of :name
 end
