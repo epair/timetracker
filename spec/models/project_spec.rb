a8c98ca88e6007951c45073d664206a54241eb38
Emmett Pair <elpair@gmail.com>
2021-03-06 18:25:41 -0500

    refactor project model spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,29 +3,26 @@ require_relative '../../lib/models/project'
 require_relative '../../lib/models/tag'
 
 RSpec.describe Project do
-  context 'has a required name field' do
-    it 'saves a project with a valid name' do
-      project = Project.new(name: 'name')
-      expect(project.save).to be_truthy
+  describe '.create' do
+    context 'with a unique valid name' do
+      it 'creates a project' do
+        project = FactoryBot.build(:project)
+        expect(project).to be_valid
+      end
     end
 
-    it 'rejects a project with no name' do
-      project = Project.new(name: nil)
-      expect(project.save).to be_falsey
+    context 'with no name' do
+      it 'rejects a project' do
+        project = FactoryBot.build(:project, name: nil)
+        expect(project).to be_invalid
+      end
     end
 
-    it 'rejects a project with an empty string name' do
-      project = Project.new(name: '')
-      expect(project.save).to be_falsey
-    end
-  end
-
-  context 'has_many tags through project_tags' do
-    it 'returns an array of tags' do
-      project = Project.create(name: 'timetracker')
-      tag = Tag.create(name: 'testing', project: project)
-
-      expect(project.tags.first).to eq(tag)
+    context 'with an empty string name' do
+      it 'rejects a project' do
+        project = FactoryBot.build(:project, name: '')
+        expect(project).to be_invalid
+      end
     end
   end
 end
